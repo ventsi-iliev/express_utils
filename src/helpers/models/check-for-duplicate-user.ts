4a00ecb6407bb764bbaae24a27524c03e004f247
Ventsislav Iliev <ventsislav.iliev@zautre.com>
2024-04-28 18:43:49 +0300

    Fix

--- src/helpers/models/check-for-duplicate-user.ts
+++ src/helpers/models/check-for-duplicate-user.ts
@@ -1,6 +1,6 @@
 import { Model } from "mongoose";
 
-export async function checkForDuplicateUser<T extends typeof Model>(model: typeof Model, email: string, password: string) {
+export async function checkForDuplicateUser<T extends typeof Model>(model: T, email: string, password: string) {
     const user = await model.findOne({ email });
 
     console.log(user);
