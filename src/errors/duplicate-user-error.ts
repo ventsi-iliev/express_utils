19e7d84bdba4dc6dd3f3f41286aa6afc4ff89948
Ventsislav Iliev <ventsislav.iliev@zautre.com>
2024-04-28 16:39:13 +0300

    Add constructor to DuplicateUserError class

--- src/errors/duplicate-user-error.ts
+++ src/errors/duplicate-user-error.ts
@@ -3,6 +3,12 @@ import { BaseError } from "./base-error";
 export class DuplicateUserError extends BaseError {
     statusCode = 403;
 
+    constructor(public errorMsg: string) {
+        super(errorMsg);
+
+        Object.setPrototypeOf(this, DuplicateUserError.prototype);
+    }
+
     parseErrors() {
         return [
             {
