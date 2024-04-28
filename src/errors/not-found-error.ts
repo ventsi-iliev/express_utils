bae3f0cc821fab36db8b9f84c4bd536396bd79c4
Ventsislav Iliev <ventsislav.iliev@zautre.com>
2024-04-28 14:51:43 +0300

    Add NotFoundError child class

---
+++ src/errors/not-found-error.ts
@@ -0,0 +1,14 @@
+import { BaseError } from "./base-error";
+
+export class NotFoundError extends BaseError {
+    statusCode = 404;
+
+    constructor(public errorMsg: string) {
+        super('Route not found');
+        Object.setPrototypeOf(this, NotFoundError.prototype);
+    }
+
+    parseErrors() {
+        return [{ message: this.errorMsg || 'Not Found' }];
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // Errors
 export * from './errors/base-error';
 export * from './errors/post-request-validation-error';
+export * from './errors/not-found-error';
 
 // Middlewares
 export * from './middlewares/error-handler';
