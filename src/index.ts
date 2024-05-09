7046eca64975d72a62487e347a6c374129a49601
Ventsislav Iliev <ventsislav.iliev@zautre.com>
2024-05-09 09:48:29 +0300

    Update

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,7 @@ export * from './helpers/models/rename-id-field';
 export * from './helpers/models/check-for-duplicate-user';
 export * from './helpers/models/check-if-login-is-valid';
 export * from './helpers/jwt/JWT';
+export * from './helpers/mongoose/mongoose-id-manager';
 
 // Interfaces
 export * from './interfaces/UserPayload';
