8032d46f3f4c0fcff078d6fc2844b191a5ea2f2f
Ventsislav Iliev <ventsislav.iliev@zautre.com>
2024-05-15 17:54:18 +0300

    Update

--- src/middlewares/process-jwt-payload.ts
+++ src/middlewares/process-jwt-payload.ts
@@ -3,20 +3,8 @@ import { JWT_Manager } from '../helpers/jwt/JWT';
 import { UserPayload } from '../interfaces/UserPayload';
 
 export const processJwtPayload = (req: Request, res: Response, next: NextFunction) => {
-    console.log(!req.headers.hasOwnProperty('Ignore-Jwt-Processing'));
 
-    if(!req.session && !req.session.token) {
-        return next();
-    }
-
-
-    if(!req.headers.hasOwnProperty('Ignore-Jwt-Processing')) {
-        const payload = JWT_Manager.verify(req, 'token', process.env.JWT_KEY!) as UserPayload;
-    
-        if(payload) {
-            req.currentUser = payload;
-        }
-    } else {
+    if(req.headers.hasOwnProperty('ignore-jwt-processing')) {
         req.currentUser = {
             id: 'fake-id',
             name: 'fake-name',
@@ -24,7 +12,19 @@ export const processJwtPayload = (req: Request, res: Response, next: NextFunctio
             email: 'fake-email@fake.bg',
             profileImage: 'fake-image'
         }
+
+        return next();
     }
 
+    if(!req.session && !req.session.token) {
+        return next();
+    }
+
+    const payload = JWT_Manager.verify(req, 'token', process.env.JWT_KEY!) as UserPayload;
+    
+    if(payload) {
+        req.currentUser = payload;
+    }
+    
     next();
 }
