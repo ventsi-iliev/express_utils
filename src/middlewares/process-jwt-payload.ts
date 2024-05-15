203cf6e4cdfe29bd4d08df5dd55c520aec8676b7
Ventsislav Iliev <ventsislav.iliev@zautre.com>
2024-05-15 17:51:41 +0300

    Update

--- src/middlewares/process-jwt-payload.ts
+++ src/middlewares/process-jwt-payload.ts
@@ -3,11 +3,12 @@ import { JWT_Manager } from '../helpers/jwt/JWT';
 import { UserPayload } from '../interfaces/UserPayload';
 
 export const processJwtPayload = (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.headers);
+    
     if(!req.session && !req.session.token) {
         return next();
     }
 
-    console.log(req.headers);
 
     if(!req.headers.hasOwnProperty('Ignore-Jwt-Processing')) {
         const payload = JWT_Manager.verify(req, 'token', process.env.JWT_KEY!) as UserPayload;
