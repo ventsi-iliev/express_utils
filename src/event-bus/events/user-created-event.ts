661b3e0cca1452bc98c357217c425f80e2e1ae18
Ventsislav Iliev <ventsislav.iliev@zautre.com>
2024-05-15 16:43:51 +0300

    Update

--- src/event-bus/events/user-created-event.ts
+++ src/event-bus/events/user-created-event.ts
@@ -3,7 +3,6 @@ import { Subjects } from "./subjects";
 export interface UserCreatedEvent {
     subject: Subjects.UserCreated,
     data: {
-        userID: string;
         username: string;
         name: string;
         profileImage?: string;
