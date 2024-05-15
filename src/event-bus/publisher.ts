e8b8276e7140ba8d901beb6f5200aa60477c92a4
Ventsislav Iliev <ventsislav.iliev@zautre.com>
2024-05-15 22:56:05 +0300

    Update

--- src/event-bus/publisher.ts
+++ src/event-bus/publisher.ts
@@ -1,6 +1,17 @@
 
-import { Stan } from 'node-nats-streaming';
+import { AckHandlerCallback, Stan } from 'node-nats-streaming';
 import { Event } from './events/base-event';
+
+export function callback<T extends { subject: string }>(this: T, data: any, resolve: any, reject: any, err: Error | undefined): AckHandlerCallback | undefined {
+    console.log(data);
+                
+    if(err) {
+        return reject(err);
+    }
+
+    console.log(`Event published. Subject: ${this.subject}!`);
+    resolve(`Event published: ${this.subject}!`);
+}
 export abstract class Publisher<T extends Event> {
     abstract subject: T['subject'];
     private client: Stan;
@@ -12,16 +23,7 @@ export abstract class Publisher<T extends Event> {
     // Add promise
     publish(data: T['data']): Promise<void | string> {
         return new Promise((resolve, reject) => {
-            this.client.publish(this.subject, JSON.stringify(data), (err) => {
-                console.log(data);
-                
-                if(err) {
-                    return reject(err);
-                }
-
-                console.log(`Event published. Subject: ${this.subject}!`);
-                resolve(`Event published: ${this.subject}!`);
-            });
+            this.client.publish(this.subject, JSON.stringify(data), callback.bind(this, data, resolve, reject));
         })
     }
 }
