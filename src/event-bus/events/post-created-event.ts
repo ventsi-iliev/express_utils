18289ec5b49608524657f5d20d449cc34810d70e
Ventsislav Iliev <ventsislav.iliev@zautre.com>
2024-05-20 20:52:10 +0300

    Update

--- src/event-bus/events/post-created-event.ts
+++ src/event-bus/events/post-created-event.ts
@@ -1,10 +1,11 @@
+import { Types } from "mongoose";
 import { Subjects } from "./subjects";
 
 export interface PostCreatedEvent {
     subject: Subjects.PostCreated;
     data: {
-        userID: string;
-        postID: string;
+        userID: Types.ObjectId;
+        postID: Types.ObjectId;
         resourceURL: string;
         type: 'image' | 'video';
         version: number;
