20b6f4d77e62075f26340f6f200f51718921572e
Ventsislav Iliev <ventsislav.iliev@zautre.com>
2024-05-25 17:28:21 +0300

    Update

---
+++ src/event-bus/events/notification-status-updated-event.ts
@@ -0,0 +1,10 @@
+import { Types } from "mongoose";
+import { Subjects } from "./subjects";
+
+export interface NotificationStatusUpdatedEvent {
+    subject: Subjects.NotificationStatusUpdated;
+    data: {
+        userID: Types.ObjectId,
+        status: 'new' | 'old'
+    }
+}
